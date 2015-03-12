636c2c8a61659c3a4e2592c819dc4f57714fa805
Tommy Gibbons <tommygibbons@Tommys-MacBook-Pro-2.local>
2015-03-12 13:57:23 -0700

    Initial Commit

---
+++ TicTac/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TicTac
+//
+//  Created by Tommy Gibbons on 3/12/15.
+//  Copyright (c) 2015 Tommy Gibbons. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TicTac/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TicTac
+//
+//  Created by Tommy Gibbons on 3/12/15.
+//  Copyright (c) 2015 Tommy Gibbons. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
